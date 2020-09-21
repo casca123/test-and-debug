4d4297e5bdf55a48a62f5b12c4b5056a9540b60b
casca123 <71664723+casca123@users.noreply.github.com>
2020-09-21 21:21:04 +0300

    Add files via upload

---
+++ lab_1/lab_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b = 7;
+
+            string str = "Hello, World!!!";
+
+            a = 123;
+
+            Console.WriteLine("Значение переменной а равно {0}",a);
+            Console.WriteLine("Значение переменной b равно {0}",b);
+            Console.WriteLine("Значение a+b равно {0}+{1} = {2}",a,b,a+b);
+            Console.WriteLine(str);
+            Console.WriteLine("\nЛабораторная работа №1 \"Создание консольного приложения\"");
+            Console.WriteLine("Выполнил: Поесков Леонид Константинович");
+            Console.WriteLine("09.03.02 ИНС-б-о-18-1");
+            Console.WriteLine("25.08.2000");
+            Console.WriteLine("Ставрополь");
+            Console.WriteLine("Физическая культура");
+            Console.WriteLine("Шоссейный велоспорт, горы, походы");
+
+            
+            double ch, E=2.7, t=0.1, f = 5.0, z = 9.0;
+
+            Console.WriteLine("\nЗначение выражения 17 варианта: {0}", ((6/f*1)*z+z*(f*1)-((f*1+E*t)/6)));
+
+          int _1_01, b100, double_1, _b200, MyVar, create, 4perem, _5elem, zo0, wodoo, UserCount, system_call, 
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
