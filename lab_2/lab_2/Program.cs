93734da1764f09eb71ad7dfd67a53fceda076620
casca123 <71664723+casca123@users.noreply.github.com>
2020-10-20 02:55:56 +0300

    Add files via upload

---
+++ lab_2/lab_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter saveout = Console.Out;
+            TextReader sevein = Console.In;
+            var outFile = new StreamWriter(@"output.txt");
+            var inFile = new StreamReader(@"input.txt");
+
+            Console.SetOut(outFile);
+            Console.SetIn(inFile);
+
+            double a, b, c, d, e, s, k;
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            d = Convert.ToDouble(Console.ReadLine());
+            e = Convert.ToDouble(Console.ReadLine());
+
+            if ((d > 1) || (b - e + a > 0))
+            {
+                s = (Math.Sqrt(b - e + a)) / (Math.Sqrt(d - 1));
+                Console.WriteLine(String.Format("{0:0.000}", s));
+            }
+            else Console.WriteLine("Error");
+
+            if ((c - 11 * b > 0) || (b - e + a > 0))
+            {
+                k = (Math.Sqrt(c - 11 * b)) / (Math.Sqrt(b - e + a));
+                Console.WriteLine(String.Format("{0:0.000}", k));
+            }
+            else Console.WriteLine("Error");
+
+            Console.SetOut(outFile);
+            outFile.Close();
+            Console.SetIn(inFile);
+            inFile.Close();
+        }
+    }
+}
